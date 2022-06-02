b3b18b4dc57c6f29b90d7435bf865dc622752ca1
Guillermonaxer <105644228+Guillermonaxer@users.noreply.github.com>
2022-06-02 17:12:21 +0200

    Update persistencia.py

--- persistencia.py
+++ persistencia.py
@@ -4,7 +4,7 @@
 def guardar_pedido(nombre, apellidos):
     """funcion que abre y escribe en un fichero"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write("-"+nombre+""+apellidos+"\n")
+        file.write("-"+nombre+" "+apellidos+"\n")
 #yo habría puesto:file.write(nombre+" "+apellidos+"\n") para separar
 #el nombre de los apellidos, pero me he limitado a copiar lo que mandastéis.
     file.close()
