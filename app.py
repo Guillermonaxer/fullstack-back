c2b59dfc72ccd7bfba0c1435c76c11dec70681a0
Guillermonaxer <105644228+Guillermonaxer@users.noreply.github.com>
2022-06-06 13:37:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ def checksize():
     """
     nombre = request.form.get("tamaño")
     if nombre == 'S':
-       mensaje = 'No disponible'
+        mensaje = 'No disponible'
     if nombre != 'S' and nombre != 'selecciona':
-       mensaje = 'Disponible'
+        mensaje = 'Disponible'
     if nombre == 'selecciona':
-       mensaje = 'Error:Debes seleccionar un tamaño'
+        mensaje = 'Error:Debes seleccionar un tamaño'
 
     return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
 
@@ -34,7 +34,7 @@ def hello():
     print(apellidos)
     # guardo nombre y apellidos en un txt(no me quedaba claro si era necesario o no)
     with open("pedidos.txt", "w", encoding="utf-8") as file:
-         file.write("")
-         file.close()
+        file.write("")
+        file.close()
     persistencia.guardar_pedido(nombre, apellidos)
     return redirect("http://localhost/master/solicita_pedido.html", code=302)
